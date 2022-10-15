2559b3d2e8fce4f55598d89c21ad2db56ec3c10f
forceHuman <ffferrari.gag@yandex.ru>
2022-10-15 09:37:38 +0300

    hw3

---
+++ CAOS_hw_3/task0.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+
+//int f(int a, int b, int c, int x);
+//int sum(int a, uint64_t y, int *b);
+int solve(int64_t A, int B, int C, int D);
+
+int main(){
+	int x[] = {1, 2};
+	int xq = 4;
+	uint64_t y = 2;  
+	//printf("%d", f(1, 2, 3, 4)); 
+	printf("%d", solve(0, 0, 0, 0));
+}
